d92e44c32b03e93c9c36a05fb0ca71fb66819d33
Brahim <brahimaz@hotmail.com>
2026-06-23 19:40:19 +0200

    change load function in predict

--- src/predict.py
+++ src/predict.py
@@ -1,14 +1,23 @@
 from src.config import DEFAULT_MODEL
 from src.data_processing import *
 from joblib import load
+from functools import lru_cache
 
-
-model=load(DEFAULT_MODEL)
+@lru_cache
+def get_model():
+    return load(DEFAULT_MODEL)
 
 def predict(df):
+    model = get_model()
     return model.predict(df)
 
 
 if __name__=='__main__':
     df=load_data()
     print(predict(df.head()))
+
+
+
+
+
+
