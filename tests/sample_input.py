9f28f934adb1e4ac04222b7aef0a3e36ec4575da
Brahim <brahimaz@hotmail.com>
2026-06-23 20:19:54 +0200

    update sample_input

--- tests/sample_input.py
+++ tests/sample_input.py
@@ -1,13 +1,16 @@
 from src.data_processing import buil_features,load_data
 
 
-
+import pandas as pd
 
 def sample_input():
-    df=load_data()
-    X , Y = buil_features(df)   
-    return X.head()
-
-
-if __name__=='__main__':
-    print(sample_input())
+    return pd.DataFrame({
+        "Pregnancies": [1, 2],
+        "Glucose": [120, 85],
+        "BloodPressure": [70, 80],
+        "SkinThickness": [20, 25],
+        "Insulin": [0, 30],
+        "BMI": [25.0, 30.0],
+        "DiabetesPedigreeFunction": [0.2, 0.4],
+        "Age": [25, 35]
+    })
