464713abae49c79b9d11d9d073cca14ef688f517
Brahim <brahimaz@hotmail.com>
2026-06-25 13:11:39 +0200

    add RandomForest and XGBoost models

--- src/config.py
+++ src/config.py
@@ -6,4 +6,5 @@ MAX_ITER=1000
 BASE_DIR=Path(__file__).resolve().parent.parent
 DATA_PATH=BASE_DIR /"data" / "raw" /"diabetes.csv"
 MODEL_PATH=BASE_DIR / "models"
-DEFAULT_MODEL =MODEL_PATH /'LogisticRegression.joblib'
+#DEFAULT_MODEL =MODEL_PATH /'LogisticRegression.joblib'
+DEFAULT_MODEL =MODEL_PATH /'RandomForestClassifier.joblib'
--- src/train.py
+++ src/train.py
@@ -3,11 +3,13 @@ from joblib import dump
 from sklearn.model_selection import train_test_split
 from sklearn.pipeline import Pipeline
 from sklearn.linear_model import LogisticRegression
+from sklearn.ensemble import RandomForestClassifier
 from src.data_processing import *
 from src.evaluate import evaluate
 import mlflow
 import mlflow.sklearn
 from mlflow.models import infer_signature
+from xgboost import XGBClassifier
 
 mlflow.set_tracking_uri("sqlite:///mlflow.db")
 mlflow.set_experiment("diabetes_experiment")
@@ -15,12 +17,26 @@ mlflow.set_experiment("diabetes_experiment")
 def train_model(model_name, model):
 
     X_train, X_test, Y_train, Y_test = get_train_test_data()
-    processor = buil_preprocessor_regression(X_train)
+    
+    # this to calculate the value of scale_pos_weight=1.87
+    neg =(Y_train==0).sum()
+    pos =(Y_train==1).sum()
 
-    pipeline = Pipeline([
-        ("preprocessor", processor),
-        ("model", model)
-    ])
+    print("negative",neg  )
+    print("positive",pos  )
+    
+    processor = None
+
+    if model_name == "lr":
+        processor = buil_preprocessor_regression(X_train)
+
+    steps = []
+
+    if processor:
+        steps.append(("preprocessor", processor))
+
+    steps.append(("model", model))
+    pipeline = Pipeline(steps)
 
     with mlflow.start_run(run_name=model_name):
 
@@ -39,21 +55,19 @@ def train_model(model_name, model):
             "model",
             model.__class__.__name__
         )
-        mlflow.log_param(
-            "random_state",
-            RANDOM_STATE
-        )
-        mlflow.log_param(
-            "max_iter",
-            MAX_ITER
-        )
-      
+              
+        mlflow.log_params(model.get_params())
+        mlflow.log_param("threshold", 0.3)
         mlflow.log_metrics({
             "accuracy": accuracy,
             "recall": recall,
             "f1score": f1score,
             "roc_auc": roc_auc
         })
+
+        mlflow.log_param("negative_samples", int(neg))
+        mlflow.log_param("positive_samples", int(pos))
+        mlflow.log_param("imbalance_ratio", float(neg / pos))
         
         signature = infer_signature(X_train, pipeline.predict(X_train))
         mlflow.sklearn.log_model(
@@ -83,7 +97,14 @@ def train_model(model_name, model):
     return pipeline, X_test, Y_test
     
 models={
-    "lr": LogisticRegression(random_state=RANDOM_STATE ,max_iter=MAX_ITER)
+    "lr": LogisticRegression(random_state=RANDOM_STATE ,max_iter=MAX_ITER),
+    "rf": RandomForestClassifier(criterion='gini', n_estimators=200, random_state=RANDOM_STATE ),
+    "xgb": XGBClassifier(
+    n_estimators=300,
+    learning_rate=0.05,
+    max_depth=6,
+    random_state=RANDOM_STATE, scale_pos_weight=1.87,
+)
 }
 
 
